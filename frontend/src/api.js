c710e3345b76e180e60b1d5a2e19f65809fc9268
Joakamakaka1 <joaquinfutbolero11@gmail.com>
2025-03-11 18:02:22 +0100

    Nuevas pruebas

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,9 +1,12 @@
 import axios from 'axios';
 
-const API_URL = 'https://react-express-mongodb-vgbd.vercel.app/';
+const API_URL = 'https://react-express-mongodb-vgbd.vercel.app/api/users';
 
 const axiosInstance = axios.create({
   baseURL: API_URL,
+  headers: {
+    'Content-Type': 'application/json',
+  }
 });
 
 export default axiosInstance;
